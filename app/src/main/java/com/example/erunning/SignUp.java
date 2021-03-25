cd79ac179f921d704a1ef6c2aa9eb18b40570aae
seunggyu97 <seunggyu97@gmail.com>
2021-03-25 10:21:44 +0900

    회원가입 실패 범위 추가(이메일 중복, 이메일 형식, 비밀번호 불일치, 6자이상구분,...)

--- app/src/main/java/com/example/erunning/SignUp.java
+++ app/src/main/java/com/example/erunning/SignUp.java
@@ -51,32 +51,59 @@ public class SignUp extends AppCompatActivity {
         String Email = ((EditText)findViewById(R.id.CreateemailEditText)).getText().toString();
         String Password = ((EditText)findViewById(R.id.CreatePassWordEditText)).getText().toString();
         String CheckPassWord = ((EditText)findViewById(R.id.CreatePassWordCheckEditText)).getText().toString();
-        // 이메일 계정 생성 시작
-        if(Password.equals(CheckPassWord)) {
-            mAuth.createUserWithEmailAndPassword(Email, Password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        createSuccess();// 생성 성공 메소드 호출
+        String ErrorEmailAlreadyUse = "com.google.firebase.auth.FirebaseAuthUserCollisionException: The email address is already in use by another account.";
+        String ErrorEmailNot = "com.google.firebase.auth.FirebaseAuthInvalidCredentialsException: The email address is badly formatted.";
 
-                                    } else {
-                                        // 실패시
-                                        Log.w(TAG, "이메일 생성 실패", task.getException());
-                                        Toast.makeText(SignUp.this, "중복된 이메일이거나 비밀번호가 너무 짧습니다. 다시 확인해 주세요.",
-                                                Toast.LENGTH_SHORT).show();
+        // 이메일 계정 생성 시작
+        if(Email.length() > 0 && Password.length() > 0 && CheckPassWord.length() > 0){
+            if(Password.length() > 5 && CheckPassWord.length() > 5){
+                if(Password.equals(CheckPassWord)) {
+                    mAuth.createUserWithEmailAndPassword(Email, Password)
+                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            if (task.isSuccessful()) {
+                                                createSuccess();// 생성 성공 메소드 호출
+
+                                            } else {
+                                                // 실패시
+                                                String Errormsg = task.getException().toString();
+                                                Log.w(TAG, "이메일 생성 실패", task.getException());
+                                                if(Errormsg.equals(ErrorEmailAlreadyUse)){
+                                                    Toast.makeText(SignUp.this, "이미 가입된 이메일입니다.",
+                                                            Toast.LENGTH_SHORT).show();
+                                                }
+                                                else if(Errormsg.equals(ErrorEmailNot)){
+                                                    Toast.makeText(SignUp.this, "올바르지 않은 이메일 형식입니다.",
+                                                            Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        }
                                     }
-                                }
-                            }
-                    );
+                            );
+                }
+                else{
+                    //비밀번호 일치하지 않는 경우
+                    Toast.makeText(SignUp.this, "비밀번호가 일치하지 않습니다.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+            else{
+                Toast.makeText(SignUp.this, "비밀번호는 최소 6자 이상 입력해주세요.",
+                        Toast.LENGTH_SHORT).show();
+            }
+
         }
         else{
-            //비밀번호 일치하지 않는 경우
-            Toast.makeText(SignUp.this, "비밀번호가 일치하지 않습니다.",
+            Toast.makeText(SignUp.this, "이메일과 비밀번호를 모두 입력해주세요.",
                     Toast.LENGTH_SHORT).show();
         }
 
+
     }
+
+
+
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
