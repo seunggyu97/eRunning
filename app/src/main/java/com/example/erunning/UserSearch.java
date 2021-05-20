8fcab12cfa065875068dcc6f664817a7946bfed0
kimseungyeon <tmddus418@naver.com>
2021-05-21 05:40:42 +0900

    5.21 유저검색에서 상대 프로필로 이동

--- app/src/main/java/com/example/erunning/UserSearch.java
+++ app/src/main/java/com/example/erunning/UserSearch.java
@@ -1,5 +1,6 @@
 package com.example.erunning;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,11 +14,14 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class UserSearch extends BasicActivity implements FirestoreAdapter.OnListItemClick {
 
     private RecyclerView recyclerView;
     private FirestoreAdapter adapter;
     private FirebaseFirestore firebaseFirestore;
+    private CircleImageView nullProfile;
 
 
 
@@ -34,7 +38,7 @@ public class UserSearch extends BasicActivity implements FirestoreAdapter.OnList
 
 
         //Query
-        Query query = firebaseFirestore.collection("users").orderBy("name"); // 데이터 정렬 orderBy
+        Query query = firebaseFirestore.collection("users").orderBy("name").startAt("s"); // 데이터 정렬 orderBy
 
         PagedList.Config config = new PagedList.Config.Builder()
                 .setInitialLoadSizeHint(10)
@@ -49,8 +53,6 @@ public class UserSearch extends BasicActivity implements FirestoreAdapter.OnList
 
         adapter = new FirestoreAdapter(options, this);
 
-
-
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
@@ -64,9 +66,31 @@ public class UserSearch extends BasicActivity implements FirestoreAdapter.OnList
         }
     };
 
+
     @Override
     public void onItemClick(DocumentSnapshot snapshot, int position) {
-        Log.d("ITEM_CLICK", "Clicked an item" + position + " and the ID :" + snapshot.getId());
+        nullProfile = findViewById(R.id.iv_otherprofileimage);
+        String name = snapshot.getData().get("name").toString();
+//        String post = snapshot.getData().get("post").toString();
+//        String follower = snapshot.getData().get("follower").toString();
+//        String following = snapshot.getData().get("following").toString();
 
+
+        Intent intent = new Intent(getApplicationContext(), OtherAccount.class);
+
+        if(snapshot.getData().get("photoUrl") != null) {
+            String image = snapshot.getData().get("photoUrl").toString();
+            intent.putExtra("userimage", image);
+        }
+        if(snapshot.getData().get("bio") != null){
+            String bio = snapshot.getData().get("bio").toString();
+            intent.putExtra("userbio", bio);
+        }
+
+        intent.putExtra("username", name);
+
+
+        startActivity(intent);
+        Log.d("ITEM_CLICK", "Clicked an item" + position + " and the ID :" + snapshot.getId() + " and username : " + snapshot.getData().get("username"));
     }
 }
